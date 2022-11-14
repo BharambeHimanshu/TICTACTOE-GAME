3df83940e66ddb093b23a43e71f238fa3d149293
Himanshu Bharambe <himanshubharambe04@gmail.com>
2022-11-14 19:47:32 +0530

    added board state and also react class

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Square({value}) {
+export default function Square({value, onClick}) {
     return(
         <>
-        <button type="button" className="square">
+        <button type="button" className="square" onClick={onClick}>
         {value}
         </button>
         </>
